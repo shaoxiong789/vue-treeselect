093ae64ab48aa0940d96756db92723872b90ac53
dingshaoxiong <you@example.com>
2019-05-08 22:54:20 +0800

    改一些细节

--- src/components/MultiValueItem.vue
+++ src/components/MultiValueItem.vue
@@ -35,7 +35,7 @@
       return (
         <div class="vue-treeselect__multi-value-item-container">
           <div class={itemClass} onMousedown={this.handleMouseDown}>
-            <span class="vue-treeselect__multi-value-label">{ labelRenderer }</span>
+            <span class="vue-treeselect__multi-value-label" title={labelRenderer.length > 10 ? labelRenderer : ''} >{ labelRenderer.length > 10 ? `${labelRenderer.slice(0, 10)}...` : labelRenderer }</span>
             <span class="vue-treeselect__icon vue-treeselect__value-remove"><DeleteIcon /></span>
           </div>
         </div>
